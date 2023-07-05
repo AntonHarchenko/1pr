81313136ad17d5d797c9712ab6526df046ae343f
AntonHarchenko <138710316+AntonHarchenko@users.noreply.github.com>
2023-07-05 21:44:42 +0300

    Update main.c

--- НПС4/main.c
+++ НПС4/main.c
@@ -4,21 +4,21 @@
 
 int main()
 {
-    SetConsoleCP(1251);
+    SetConsoleCP(1251); 
     SetConsoleOutputCP(1251);
     int n, t = 0;
-    printf("Ââåä³òü íàòóðàëüíå ÷èñëî n: ");
-    scanf("%d", &n);
-    if (n > 150 || n < 1)
-        printf("Error");
+    printf("Ð’Ð²ÐµÐ´Ñ–Ñ‚ÑŒ Ð½Ð°Ñ‚ÑƒÑ€Ð°Ð»ÑŒÐ½Ðµ Ñ‡Ð¸ÑÐ»Ð¾ n: ");
+    scanf("%d", &n); 
+    if (n > 150 || n < 1) 
+        printf("Error"); 
     else
     {
-        for (int i = 1; i <= n; i++)
+        for (int i = 1; i <= n; i++) 
         {
-            if (n % i == n / i)
-                t++;
+            if (n % i == n / i) 
+                t++; 
         }
-    printf("Ê³ëüê³ñòü ð³âíèõ ä³ëüíèê³â ÷èñëà %d: %d\n", n, t);
+    printf("ÐšÑ–Ð»ÑŒÐºÑ–ÑÑ‚ÑŒ Ñ€Ñ–Ð²Ð½Ð¸Ñ… Ð´Ñ–Ð»ÑŒÐ½Ð¸ÐºÑ–Ð² Ñ‡Ð¸ÑÐ»Ð° %d: %d\n", n, t); 
     }
     return 0;
 }
